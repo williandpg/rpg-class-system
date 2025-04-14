b5e33e81a5de8444f385ee70d2a35ea23561f2de
Willian Gonçalves <willian.p.g@outlook.com>
2025-04-14 12:07:12 -0300

    Add new heroes

--- hero-class.js
+++ hero-class.js
@@ -28,5 +28,11 @@ class heroi{
 }
 
 const meuHeroi = new heroi("Gandalf", 1000, "mago");
+const meuHeroi2 = new heroi("Aragorn", 87, "guerreiro");
+const meuHeroi3 = new heroi("Goku", 30, "monge");
+const meuHeroi4 = new heroi("Naruto", 16, "ninja");
 
 console.log(`O ${meuHeroi.tipo} atacou usando ${meuHeroi.ataque()}`);
+console.log(`O ${meuHeroi2.tipo} atacou usando ${meuHeroi2.ataque()}`);
+console.log(`O ${meuHeroi3.tipo} atacou usando ${meuHeroi3.ataque()}`);
+console.log(`O ${meuHeroi4.tipo} atacou usando ${meuHeroi4.ataque()}`);
